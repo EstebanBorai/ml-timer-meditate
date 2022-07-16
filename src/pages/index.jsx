11f2be531d86873e9bdfe922a42bbe0581c65ec9
Esteban Borai <estebanborai@gmail.com>
2022-07-15 22:56:59 -0400

    fix: set interval and minor refactor

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,50 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const SECOND_IN_MS = 1000;
 
 export default function Home() {
   const [soundBeforeFinishing, setSoundBeforeFinishing] = useState(true);
-  const [startTimer, setStartTime] = useState(false);
-  const [time, setTime] = useState(2000);
+  const [isRunning, setIsRunning] = useState(false);
+  const [seconds, setSeconds] = useState(10);
+  const [intervalId, setIntervalId] = useState(null);
 
-  function beginTimer() {
-    setStartTime(true);
-    const timer = setInterval(() => {
-      setTime(time - 1000);
-      console.log(time);
-      if (time === 0) {
-        clearInterval(timer);
-        setTime(2000);
+  useEffect(() => {
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
       }
-    }, 1000);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (seconds <= 0 && intervalId && isRunning) {
+      setIsRunning(false);
+      clearInterval(intervalId);
+      setSeconds(0);
+    }
+  }, [seconds, intervalId, isRunning]);
+
+  const start = () => {
+    setIsRunning(true);
+
+    const id = setInterval(() => {
+      setSeconds((val) => val - 1);
+    }, SECOND_IN_MS);
+
+    setIntervalId(id);
   }
 
-  function cancelTimer() {
-    setStartTime(false);
+  const cancel = () => {
+    setIsRunning(false);
+    clearInterval(intervalId);
+    setSeconds(10);
   }
 
   return (
     <>
       <div className="text-center h-screen">
         <h1 className="text-5xl text-left mt-10 ml-10">Timer To Meditate</h1>
-        <p className="mt-24 text-9xl">{time / 1000}</p>
+        <p className="mt-24 text-9xl">{seconds === 0 ? '-' : seconds}</p>
         <p className="text-xs">Make a sound a minute before finishing</p>
         <div className="mt-2">
           {soundBeforeFinishing ? (
@@ -45,21 +64,12 @@ export default function Home() {
           )}
         </div>
         <div className="mt-20">
-          {startTimer ? (
-            <button
-              onClick={cancelTimer}
-              className="py-2 px-7 text-3xl rounded-3xl border-2 border-rose-600"
-            >
-              Cancel
-            </button>
-          ) : (
-            <button
-              onClick={beginTimer}
-              className="py-2 px-7 text-3xl rounded-3xl border-2 border-green-600"
-            >
-              Begin
-            </button>
-          )}
+          <button
+            onClick={isRunning ? cancel : start}
+            className={'py-2 px-7 text-3xl rounded-3xl border-2 border-green-600' + isRunning ? ' !border-rose-600' : ''}
+          >
+            {isRunning ? 'Cancel' : 'Start'}
+          </button>
         </div>
         <div className="mt-48"></div>
       </div>
